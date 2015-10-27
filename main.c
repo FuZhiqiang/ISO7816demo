8f7aaab3e64eed03a8432276ea395e0aac15ab91
Fu Zhiqiang <fuzhiqiang76@hotmail.com>
2015-10-27 22:34:48 +0800

    add T=0 transmission protocol functions

--- main.c
+++ main.c
@@ -61,10 +61,30 @@ const clock_manager_user_config_t g_defaultClockConfigRun =
         .enableInStop = false, // OSCERCLK disable in STOP mode.
     }
 };
-char tpduGetC1[]={0x00, 0x84, 0x00, 0x00, 0x04};	//TPDU of Get Challenge (get a 32-bit random number from IC card)
+
+const uint8_t apduGetC1[]={0x00, 0x84, 0x00, 0x00, 0x04};	//APDU of Get Challenge Command(get a 32-bit random number from IC card)
+uint8_t apduResponse[256];
+
+#define PROCEDURE_NULL  0x60
+#define PROCEDURE_SW1   0x90
+typedef enum{
+    IDLE, TRANS_HEADER, TRANS_PROCEDURE, TRANS_DATA, TRANS_SW1, TRANS_SW2
+}trans_step_t;
+typedef struct{
+    trans_step_t step;
+    uint8_t *pCmd;
+    uint8_t *pSend;
+    uint8_t len;
+}trans_state_t;
+trans_state_t stateTrans = {.step = IDLE,};
+
+int SendCommand(uint8_t *apduCmd, uint8_t length);
+void ResponseCallback(uint8_t *apduRsp, uint8_t length);
 
 int main(void)
 {
+    uint32_t iTimeout;
+    
 	SIM_HAL_EnableClock(SIM,kSimClockGatePortA);
 	SIM_HAL_EnableClock(SIM,kSimClockGatePortB);
     CLOCK_SYS_SetConfiguration(&g_defaultClockConfigRun);
@@ -73,17 +93,120 @@ int main(void)
 	Iso7816Init();
 	Iso7816Activate();
 
-	LPUART_DRV_SendData(FSL_LPUARTCOM1, tpduGetC1, 5);
-    // Send/receive non blocking function
+    if(0 == SendCommand((uint8_t*)apduGetC1, 5))
+    {
+        iTimeout = OSA_TimeGetMsec() + iMaxWaitTimeMs;
+    }
+    
     while(true)
     {
-		if(iUartRxTime)	//If there is data come from UART
-		{
-			if(2 < OSA_TimeDiff(iUartRxTime, OSA_TimeGetMsec()))	//If one data frame complete
-			{
-				UartStartRxNextFrame();
-			}
-		}
+        if(IDLE != stateTrans.step)
+        {
+            if(iUartRxTime)	//If there is data come from UART
+            {
+                iTimeout = iUartRxTime + iMaxWaitTimeMs;
+                iUartRxTime = 0;
+            }
+            else if(iTimeout < OSA_TimeGetMsec())
+            {
+                UartStartRxNextFrame();
+                stateTrans.step = IDLE; 
+            }
+        }
     }
 
+}
+
+int SendCommand(uint8_t *apduCmd, uint8_t length)
+{
+    assert(apduCmd);
+    assert(length >= 5);
+    
+    if(IDLE == stateTrans.step)
+    {
+        stateTrans.step = TRANS_HEADER;
+        stateTrans.pCmd = apduCmd;
+        LPUART_DRV_SendData(FSL_LPUARTCOM1, apduCmd, 5);
+        stateTrans.pSend = apduCmd + 5;
+        stateTrans.len = length - 5;
+        stateTrans.step = TRANS_PROCEDURE;
+    }
+    else
+    {
+        return 1;
+    }
+    
+    return 0;
+}
+
+void TransT0Proc(uint8_t charRx)
+{
+    static int i;
+
+    if(TRANS_PROCEDURE == stateTrans.step)
+    {
+        if(PROCEDURE_NULL == charRx)
+        {
+            return;
+        }
+        else if((PROCEDURE_SW1 == (charRx & 0xF0))
+                || (PROCEDURE_NULL == (charRx & 0xF0)))
+        {
+            apduResponse[i] = charRx;
+            i++;
+            stateTrans.step = TRANS_SW2;
+        }
+        else if(charRx == stateTrans.pCmd[1])
+        {
+            if(stateTrans.len)
+            {
+                LPUART_DRV_SendData(FSL_LPUARTCOM1, stateTrans.pSend, stateTrans.len);
+                stateTrans.len = 0;
+            }
+            if(stateTrans.pCmd[4])
+            {
+                stateTrans.step = TRANS_DATA;
+            }
+            else
+            {
+                stateTrans.step = TRANS_SW1;
+            }
+        }
+        else if(charRx == (~stateTrans.pCmd[1]))
+        {
+            if(stateTrans.len)
+            {
+                LPUART_DRV_SendData(FSL_LPUARTCOM1, stateTrans.pSend, 1);
+                stateTrans.pSend++;
+                stateTrans.len--;
+            }
+        }
+    }
+    else if(TRANS_DATA == stateTrans.step)
+    {
+        apduResponse[i] = charRx;
+        i++;
+        if(stateTrans.pCmd[4] == i)
+        {
+            stateTrans.step = TRANS_SW1;
+        }
+    }
+    else if(TRANS_SW1 == stateTrans.step)
+    {
+        apduResponse[i] = charRx;
+        i++;
+        stateTrans.step = TRANS_SW2;
+    }
+    else if(TRANS_SW2 == stateTrans.step)
+    {
+        apduResponse[i] = charRx;
+        ResponseCallback(apduResponse, i+1);
+        i = 0;
+        stateTrans.step = IDLE;
+    }
+}
+
+void ResponseCallback(uint8_t *apduRsp, uint8_t length)
+{
+    //write application codes here to deal with the response APDU
 }
